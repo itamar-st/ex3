6e66de0a126d90c541a8a55b964754067c13d31c
itamar <itamar0109@gmail.com>
2021-11-26 18:36:46 +0200

    scanf and print working

--- func_select.s
+++ func_select.s
@@ -1,7 +1,49 @@
     .file   "ex3.c"
     .align 8
     .section .rodata
+    format_input1:  .string "%d"
+    format_input2:  .string "%s"
+    format_print:  .string "(%d)\n"
     .text
+    .globl run_main
+    .type run_main, @function
+
+run_main:
+    pushq   %rbp    #setup
+    movq    %rsp, %rbp  #setup
+    subq    $16, %rsp   #create space for 1 arg
+
+    movq    $format_input1, %rdi    #format in first arg
+    leaq    -8(%rbp), %rsi  #location to save in second arg
+    xorq    %rax,%rax  #rax=0
+    call    scanf
+#   incq    -8(%rbp)    #add byte for the /0
+
+#  subq    -8(%rbp), %rsp  # open more place on the stack acordingto the number entered
+
+#   movq    $format_input2, %rdi    #format in first arg(
+#   leaq    -8(%rbp), %rsi  #location to save in second arg
+#   xorq     %rax,%rax  #rax=0
+#   call    scanf
+
+#   movq    -8(%rbp), %r11
+
+#    movq    $format_input2, %rdi    #format in first arg
+#    leaq    -8(%rbp), %rsi  #location to save in second arg
+#    xorq     %rax,%rax  #rax=0
+#    call    scanf
+
+    movq    -8(%rbp), %rsi
+    movq    $format_print, %rdi
+    xorq    %rax, %rax
+    call    printf
+
+    movq    %rbp, %rsp
+    popq    %rbp
+    xorq    %rax, %rax
+    ret
+
+    .globl run_func
     .type run_func, @function
 
 run_func:
@@ -9,42 +51,18 @@ run_func:
         movq    %rsp, %rbp  #setup
         subq    $24, %rsp #create space for 3 args
 
-        movl    %edi, %eax  #eax = op
-        sube    $50, %eax   #noralize the inputs 1 to 10
-        cmpl    $10, %edi    #cmp op,10
-        ja      .ERR        #if > goto ERR
-        je      .L60        #if = go to L60
-        jmp     *.TABLE(,%eax,4)    #else goto table[op]
 
-.ERR:
-    ret
+        movl    %edi, %eax  #eax = op
+        sub     $50, %eax   #noralize the inputs 1 to 10
+        cmpl    $10, %edi    #cmp op,10
+        #ja      .L57        #if > goto ERR
+        #je      .L60        #if = go to L60
+        #jmp     *.TABLE(,%eax,4)    #else goto table[op]
+        movq    %rbp, %rsp
+        popq    %rbp
+        xorq    %rax, %rax
+        ret
+
+
 #setting up a table with to hold all the possible numbers
 #there are more lables then choosing options but the 5 more lables is faster then using modulu 10
-.TABLE:
-    .long .L50 #Op = 0
-    .long .L51 #Op = 1
-    .long .L52 #Op = 2
-    .long .L53 #Op = 3
-    .long .L54 #Op = 4
-    .long .L55 #Op = 5
-    .long .L56 #Op = 6
-    .long .L57 #Op = 7
-    .long .L58 #Op = 8
-    .long .L59 #Op = 9
-    .long .L60 #Op = 10
-    .long .DEF #Op = 11
-
-.l50:
-    ret
-.l51:
-    ret
-.l52:
-    ret
-.l53:
-    ret
-.l54:
-    ret
-.l55:
-    ret
-.l60:
-    ret
--- run_main-test.c
+++ run_main-test.c
@@ -3,25 +3,26 @@
 
 void run_func(int opt, Pstring *ptr, Pstring *ptr1);
 
-void run_main() {
-
-	Pstring p1;
-	Pstring p2;
-	int len;
-	int opt;
-
-	// initialize first pstring
-	scanf("%d", &len);
-	scanf("%s", p1.str);
-	p1.len = len;
-
-	// initialize second pstring
-	scanf("%d", &len);
-	scanf("%s", p2.str);
-	p2.len = len;
-
-	// select which function to run
-	scanf("%d", &opt);
-	run_func(opt, &p1, &p2);
-
-}
+//void run_main();
+//{
+//
+//    Pstring p1;
+//	Pstring p2;
+//	int len;
+//	int opt;
+//
+//	// initialize first pstring
+//	scanf("%d", &len);
+//	scanf("%s", p1.str);
+//	p1.len = len;
+//
+//	// initialize second pstring
+//	scanf("%d", &len);
+//	scanf("%s", p2.str);
+//	p2.len = len;
+//
+//	// select which function to run
+//	scanf("%d", &opt);
+//	run_func(5, &p1, &p2);
+//
+//}
